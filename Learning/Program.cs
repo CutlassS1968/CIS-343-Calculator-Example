152a4d04359da7cbba984d7232fbda50febd8bed
evanjohns <evanlloydjohns@gmail.com>
2022-04-15 13:13:39 -0400

    Initial check-in of module Learning

---
+++ Learning/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Learning
+{
+    class Program
+    {
+        private Student student;
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+        }
+
+        public Program()
+        {
+            student = new Student();
+            System.Console.WriteLine(student.getNumber());
+
+            student.setNumber(1);
+            System.Console.WriteLine(student.getNumber());
+        }
+    }
+
+    class Student
+    {
+        private int number;
+        public Student()
+        {
+            number = 0;
+        }
+
+        public int getNumber()
+        {
+            return number;
+        }
+
+        public void setNumber(int number)
+        {
+            this.number = number;
+        }
+    }
+}
